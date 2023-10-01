dab3b6a8916110eafe05e5a1e6f6fde99f454ab0
Cameron Skaggs <cameronjskaggs@gmail.com>
2023-10-01 16:43:18 -0700

    Clearning up project structure

--- tests/test_backup_monitor.py
+++ tests/test_backup_monitor.py
@@ -6,11 +6,11 @@ class TestBackupMonitor(unittest.TestCase):
 
     def test_backup_script(self):
 
-        subprocess.run(["python3", "backup-monitor.py"])
+        subprocess.run(["python3", "backup-monitor.py", "-o", "input/Old.sha1.txt", "-n", "input/New.sha1.txt"])
         self.assertTrue(filecmp.cmp("OldNotInNew.txt", "expected_output/OldNotInNew_1.txt"))
         self.assertTrue(filecmp.cmp("NewNotInOld.txt", "expected_output/NewNotInOld_1.txt"))
 
-        subprocess.run(["python3", "backup-monitor.py", "-o" "Old-load-test.sha1.txt", "-n", "New-load-test.sha1.txt", "-or", "LoadOldNotInNew.txt", "-nr", "LoadNewNotInOld.txt"])
+        subprocess.run(["python3", "backup-monitor.py", "-o" "input/Old-load-test.sha1.txt", "-n", "input/New-load-test.sha1.txt", "-or", "LoadOldNotInNew.txt", "-nr", "LoadNewNotInOld.txt"])
         self.assertTrue(filecmp.cmp("LoadOldNotInNew.txt", "expected_output/OldNotInNew_load.txt"))
         self.assertTrue(filecmp.cmp("LoadNewNotInOld.txt", "expected_output/NewNotInOld_load.txt"))
     
